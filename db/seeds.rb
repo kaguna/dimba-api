3f81a9f6e0800b412e82077650002fca813d1ae9
Kevin Samoei <samoeikev@gmail.com>
2018-08-07 17:45:49 +0300

    Setup seed data for the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+20.times do
+  Team.create(
+    name: Faker::Football.team,
+    description: Faker::Lorem.sentence,
+    location: Faker::Address.city,
+    nickname: Faker::Hobbit.character
+  )
+end
+
+teams = Team.all.pluck(:id)
+teams.each do |id|
+  15.times do
+    Player.create(
+      first_name: Faker::Name.first_name, 
+      second_name: Faker::Name.middle_name,
+      last_name: Faker::Name.last_name,
+      nick_name: Faker::Football.player,
+      id_number: Faker::IDNumber.spanish_citizen_number,
+      dob: Faker::Date.birthday(19, 38),
+      phone_number: Faker::PhoneNumber.cell_phone,
+      team_id: id
+    )
+  end
+end

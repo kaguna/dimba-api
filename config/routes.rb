90e0b535ec8776b9b8e9321eda0afae8d7820282
James Kariuki Kaguna <karyorkir@gmail.com>
2018-08-09 17:21:46 +0300

    feat(teamsPage): enable single team retrieval and editing (#10)
    -create update controller.
    -create single team retrieval controller.
    -test update team.
    -test specific team retrieval.
    
    [Finishes]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   scope "teams" do
-    get "/" => "teams#show"
+    get "/" => "teams#index"
+    get "/show/:team_id" => "teams#show"
     post "/create" => "teams#create"
+    put "/edit/:team_id" => "teams#update"
   end
 end

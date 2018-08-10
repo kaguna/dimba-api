bfa69a5e287b6d056cac10d85eae53a5c0ee4cf3
James Kariuki Kaguna <karyorkir@gmail.com>
2018-08-10 16:37:42 +0300

    Revert "feat(teamPage): enable team creation (#1)"
    This reverts commit ebd610c82090cc5b8b9b794205a7f27654262331.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,13 +1,13 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'simplecov'
-if ENV['CI']
-  require 'coveralls'
-  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-  Coveralls.wear!('rails')
+SimpleCov.start :rails do
+  add_filter "db/"
+  add_filter "/app/channels/"
+  add_filter "/app/jobs/"
+  add_filter "/app/mailers/"
+  add_filter "config/"
 end
-SimpleCov.start 'rails'
->>>>>>> feat(teamPage): enable team creation (#1)
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production

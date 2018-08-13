dfa48259f39ff0c1f4cade066747b1a0b447cf89
James Kariuki <karyorkir@gmail.com>
2018-08-13 10:54:01 +0300

    chore(teamsPage): change the route patterns and use double quotes.
    -change the routing pattern.
    -use double quotes instead of single quotes.
    
    [finishes #159652505]

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   scope "teams" do
     get "/" => "teams#index"
-    get "/show/:team_id" => "teams#show"
-    post "/create" => "teams#create"
-    put "/edit/:team_id" => "teams#update"
-    delete "/delete/:team_id" => "teams#destroy"
+    get "/:team_id" => "teams#show"
+    post "/" => "teams#create"
+    put "/:team_id" => "teams#update"
+    delete "/:team_id" => "teams#destroy"
   end
 end

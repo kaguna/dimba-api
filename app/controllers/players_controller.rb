550d8f52e7bcd8157c216ac618d5dcb6765607c5
James Kariuki <karyorkir@gmail.com>
2018-08-16 02:03:59 +0300

    feat(playersPage): enable CRUD operations on a player. -create a controller file to perform CRUD operations. -write a faker for player details. -test the CRUD operations
    [finishes #159732732]

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,75 @@
+class PlayersController < ApplicationController
+  before_action :set_player, only: [:show, :update, :destroy]
+
+  def index
+    players = Player.where(team_id: params[:team_id])
+    if players.empty?
+      render json: { errors: "The team does not exist" },
+             status: :bad_request
+
+    else
+      render json: players, status: :ok
+    end
+  end
+
+  def show
+    if @player
+      render json: @player, status: :ok
+
+    else
+      render json: { errors: "The player does not exist" },
+             status: :bad_request
+    end
+  end
+
+  def create
+    create_player = Player.new(player_params)
+
+    if create_player.save
+      render json: create_player, status: :created
+
+    else
+      render json: create_player.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @player
+      @player.update_attributes(player_params)
+      render json: @player, status: :ok
+
+    else
+      render json: { errors: "The player does not exist" },
+             status: :bad_request
+    end
+  end
+
+  def destroy
+    if @player
+      @player.destroy
+      render json: { message: "Player was successfully deleted" }, status: :ok
+
+    else
+      render json: { errors: "The player does not exist" }, status: :bad_request
+    end
+  end
+
+  private
+
+  def set_player
+    @player = Player.find_by(id: params[:player_id], team_id: params[:team_id])
+  end
+
+  def player_params
+    params.permit(
+        :first_name,
+        :second_name,
+        :last_name,
+        :nick_name,
+        :id_number,
+        :dob,
+        :phone_number,
+        :team_id
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,12 @@ Rails.application.routes.draw do
     put "/edit/:team_id" => "teams#update"
     delete "/delete/:team_id" => "teams#destroy"
   end
+
+  scope "team/:team_id" do
+    get "/players" => "players#index"
+    get "/player/:player_id" => "players#show"
+    post "/player" => "players#create"
+    put "/player/:player_id" => "players#update"
+    delete "/player/:player_id" => "players#destroy"
+  end
 end

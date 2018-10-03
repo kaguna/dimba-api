3bca35ee8a1a287bb749a8b129ffe8df791ea7e0
kaguna <karyorkir@gmail.com>
2018-10-03 18:37:38 +0300

    feat(leagueFixturePage): create league fixture table
    -add relationship to the leagues table.
    -add relationship to the fixtures table.
    -modify leagues teams table and add new relationships
    
    [Finishes #160941688]

--- db/migrate/20180924152014_add_foreign_keys.rb
+++ db/migrate/20180924152014_add_foreign_keys.rb
@@ -1,6 +1,6 @@
 class AddForeignKeys < ActiveRecord::Migration[5.2]
   def change
-    add_foreign_key :leagues, :leagues_teams, column: :leagues_teams_id
+    add_foreign_key :leagues_teams, :leagues, column: :league_id
     add_foreign_key :leagues_teams, :teams, column: :team_id
   end
 end

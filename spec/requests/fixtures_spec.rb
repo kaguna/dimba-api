960ba61beccf406569851d00c62387ded1098a3b
kaguna <karyorkir@gmail.com>
2018-10-24 16:05:14 +0300

    feat(fixturePage): Save the autogenerated fixtures in one click
    -add fixture key to hold all the parameters.
    -fix fixtures tests
    
    [Finishes #161145959]

--- spec/requests/fixtures_spec.rb
+++ spec/requests/fixtures_spec.rb
@@ -32,17 +32,26 @@ RSpec.describe Fixture, type: :request do
   end
 
   let(:league_id) { league.id }
+
   let(:team_id) { team.first.id }
+
   let(:fixture_id) { fixtures.first.id }
-  let(:fixture_params) do
+
+  let(:fixture_params) {
     {
-      "home_team": team.last.id,
-      "away_team": team.first.id,
-      "league_id": league.id,
-      "season": "2017/2018",
-      "match_day": "2018-09-09",
+      fixtures: [
+        {
+          "home_team": team.last.id,
+          "away_team": team.first.id,
+          "league_id": league.id,
+          "season": "2017/2018",
+          "match_day": "2018-09-09"
+        }
+      ]
     }
-  end
+  }
+
+  let(:games) { fixture_params.size }
 
   describe "POST /league/:league_id/fixtures" do
     context "when the request is valid" do
@@ -53,7 +62,7 @@ RSpec.describe Fixture, type: :request do
       end
 
       it "creates a new fixture" do
-        expect(json.size).to eq 11
+        expect(json["message"]).to eq("#{games} games successfully created")
       end
 
       it "returns status code 201" do

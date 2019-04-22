f86790400e63c9afe7ca8857eea6249b1d15b0e1
James Kariuki Kaguna <karyorkir@gmail.com>
2019-04-22 19:21:00 +0300

    chore(routesPage): API  versioning (#49)
    -add an alias to routes
    -version all the controllers
    -fix all request tests
    
    [Finish #161341636]

--- spec/requests/fixture_squad_spec.rb
+++ spec/requests/fixture_squad_spec.rb
@@ -16,19 +16,13 @@ RSpec.describe FixtureSquad, type: :request do
 
   let!(:league) { create(:league) }
 
-<<<<<<< HEAD
   let!(:season) { create(:season) }
 
-=======
->>>>>>> feat(fixtureSquadPage): Enable users perform CRUD operations  on fixture_squad. (#47)
   let!(:fixture) do
     create(:fixture,
            home_team: teams.last.id,
            away_team: teams.first.id,
-<<<<<<< HEAD
            season_id: season.id,
-=======
->>>>>>> feat(fixtureSquadPage): Enable users perform CRUD operations  on fixture_squad. (#47)
            league_id: league.id
     )
   end
@@ -72,18 +66,12 @@ RSpec.describe FixtureSquad, type: :request do
   describe "POST league/:league_id/fixture/:fixture_id/team/team_id/squad" do
     context "when the request is valid" do
       before do
-<<<<<<< HEAD
         post api_v1_add_fixture_squad_path(leagues_id: league_id,
            fixtures_id: fixture_id,
            teams_id: team_id
            ),
             headers: authenticated_header(user),
             params: fixture_squad_params
-=======
-        post "/league/#{league_id}/fixture/#{fixture_id}/team/#{team_id}/squad",
-             headers: authenticated_header(user),
-             params: fixture_squad_params
->>>>>>> feat(fixtureSquadPage): Enable users perform CRUD operations  on fixture_squad. (#47)
       end
 
       it "creates a new fixture squad" do
@@ -100,14 +88,10 @@ RSpec.describe FixtureSquad, type: :request do
     context "when the request is valid" do
 
       before do
-<<<<<<< HEAD
         get api_v1_fixture_squads_path(leagues_id: league_id,
           fixtures_id: fixture_id,
           teams_id: team_id
           )
-=======
-        get "/league/#{league_id}/fixture/#{fixture_id}/team/#{team_id}/squad"
->>>>>>> feat(fixtureSquadPage): Enable users perform CRUD operations  on fixture_squad. (#47)
       end
 
       it "returns a list with 10 hashes" do
@@ -122,15 +106,11 @@ RSpec.describe FixtureSquad, type: :request do
     context "when the request is invalid" do
       let!(:squad_id) { 1000 }
       let(:url) do
-<<<<<<< HEAD
         api_v1_fixture_squad_path(leagues_id: league_id,
           fixtures_id: fixture_id,
           teams_id: team_id,
           fixture_squad_id: squad_id
           )
-=======
-        "/league/#{league_id}/fixture/#{fixture_id}/team/#{team_id}/squad/#{squad_id}"
->>>>>>> feat(fixtureSquadPage): Enable users perform CRUD operations  on fixture_squad. (#47)
       end
 
       before do
@@ -150,15 +130,11 @@ RSpec.describe FixtureSquad, type: :request do
   describe "DELETE fixture/:fixture_id/team/team_id/squad/:squad_id" do
     context "when the request is made by an admin" do
       before do
-<<<<<<< HEAD
         delete api_v1_delete_fixture_squad_path(leagues_id: league_id,
           fixtures_id: fixture_id,
           teams_id: team_id,
           fixture_squad_id: squad_id
           ),
-=======
-        delete "/league/#{league_id}/fixture/#{fixture_id}/team/#{team_id}/squad/#{squad_id}",
->>>>>>> feat(fixtureSquadPage): Enable users perform CRUD operations  on fixture_squad. (#47)
                headers: authenticated_header(user)
       end
 
@@ -174,15 +150,11 @@ RSpec.describe FixtureSquad, type: :request do
     context "when the request is invalid" do
       let!(:squad_id) { 1000 }
       let(:url) do
-<<<<<<< HEAD
         api_v1_delete_fixture_squad_path(leagues_id: league_id,
           fixtures_id: fixture_id,
           teams_id: team_id,
           fixture_squad_id: squad_id
         )
-=======
-        "/league/#{league_id}/fixture/#{fixture_id}/team/#{team_id}/squad/#{squad_id}"
->>>>>>> feat(fixtureSquadPage): Enable users perform CRUD operations  on fixture_squad. (#47)
       end
 
       before do
@@ -202,15 +174,11 @@ RSpec.describe FixtureSquad, type: :request do
   describe "PUT fixture/:fixture_id/team/team_id/squad/:squad_id" do
     context "when the request is valid" do
       let(:url) do
-<<<<<<< HEAD
         api_v1_edit_fixture_squad_path(leagues_id: league_id,
           fixtures_id: fixture_id,
           teams_id: team_id,
           fixture_squad_id: squad_id
         )
-=======
-        "/league/#{league_id}/fixture/#{fixture_id}/team/#{team_id}/squad/#{squad_id}"
->>>>>>> feat(fixtureSquadPage): Enable users perform CRUD operations  on fixture_squad. (#47)
       end
       before do
         put url, headers: authenticated_header(user)
@@ -228,15 +196,11 @@ RSpec.describe FixtureSquad, type: :request do
     context "when the request is invalid" do
       let!(:squad_id) { 1000 }
       let(:url) do
-<<<<<<< HEAD
         api_v1_edit_fixture_squad_path(leagues_id: league_id,
           fixtures_id: fixture_id,
           teams_id: team_id,
           fixture_squad_id: squad_id
         )
-=======
-        "/league/#{league_id}/fixture/#{fixture_id}/team/#{team_id}/squad/#{squad_id}"
->>>>>>> feat(fixtureSquadPage): Enable users perform CRUD operations  on fixture_squad. (#47)
       end
 
       before do

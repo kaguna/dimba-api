3ead7cdc53706c23e437cdadf742b96911366f6c
kaguna <karyorkir@gmail.com>
2019-05-20 17:01:30 +0300

    Enable CRUD operations on results of specific fixture

---
+++ app/controllers/api/v1/results_controller.rb
@@ -0,0 +1,62 @@
+class Api::V1::ResultsController < Api::V1::ApplicationController
+  before_action :authenticate_current_user, except: %i(index show)
+  before_action :specific_result, only: %i(update destroy)
+
+  def index
+    render json: @result, status: :ok
+  end
+
+  def show
+    render json: specific_result, status: :ok
+  end
+
+  def create
+    add_fixture_results
+  end
+
+  def update
+    specific_result.update_attributes(result_params)
+    render json: specific_result
+  end
+
+  def destroy
+    authorize specific_result
+    specific_result.destroy
+  end
+
+  private
+
+  def add_fixture_results
+    result_data.each do |attributes|
+      result = Result.new(fixture_result_params(attributes))
+      result.fixtures_id = params[:fixtures_id]
+      result.save
+    end
+  end
+
+  def result_data
+    params.require(:fixture_results)
+  end
+
+  def fixture_result_params(attributes)
+    attributes.permit(
+        :teams_id,
+        :total_goals,
+        :points
+    )
+  end
+
+  def specific_result
+    @result = Result.find(params[:result_id])
+  end
+
+  def result_params
+    params.permit(
+        :fixtures_id,
+        :teams_id,
+        :total_goals,
+        :points,
+        :league_id
+    )
+  end
+end

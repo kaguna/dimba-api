0a5adf33562cee096412cf74bbebbd7ed9899ef4
James Kariuki Kaguna <karyorkir@gmail.com>
2019-06-14 18:56:35 +0300

    Changes made to enable hosting (#63)
    * Changes made to enable hosting
    
    * Fix failing tests
    
    * Add asset precompiler

--- spec/models/fixture_squad_spec.rb
+++ spec/models/fixture_squad_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe FixtureSquad, type: :model do
   describe "Validations" do
 
     context "when inputs must be present" do
-      it { should validate_presence_of(:players_id) }
-      it { should validate_presence_of(:teams_id) }
-      it { should validate_presence_of(:fixtures_id) }
+      it { should validate_presence_of(:player_id) }
+      it { should validate_presence_of(:team_id) }
+      it { should validate_presence_of(:fixture_id) }
     end
   end
 end

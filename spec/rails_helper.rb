af266c0a909868ed335b011806e7166046fb4000
kaguna <karyorkir@gmail.com>
2019-06-28 18:03:00 +0300

    require faker

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'faker'
 require 'simplecov'
 SimpleCov.start :rails do
   add_filter "db/"

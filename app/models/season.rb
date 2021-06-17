172a065122076c5f0cddfd92571d51358220618f
kaguna <karyorkir@gmail.com>
2021-06-17 13:18:45 +0300

    end season 2 weeks after all matches are played

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,14 +1,29 @@
 class Season < ApplicationRecord
+  has_many :fixtures
+  belongs_to :league
+  has_many :leagues_teams
+  has_many :teams, through: :leagues_teams
+  
   validates :name, presence: true, allow_blank: false
 
   validates :name, uniqueness: { scope: :league_id, message: "Season name exists in this league!" }
 
   validates_uniqueness_of :league_id, conditions: -> { current }, message: "Season in progress!"
 
-  has_many :fixtures
-  belongs_to :league
-  has_many :leagues_teams
-  has_many :teams, through: :leagues_teams
   
   scope :current, -> { where(current: true) }
+
+  after_initialize :end_season!, if: :season_eligible_for_ending?
+
+  def end_season!
+    update!(current: false)
+  end
+
+  private
+
+  def season_eligible_for_ending?
+    fixtures.empty? ? false :
+     (!fixtures.pluck(:played).include?(false) &&
+      fixtures.order(:match_day).pluck(:match_day).last <= Date.today+15)
+  end
 end

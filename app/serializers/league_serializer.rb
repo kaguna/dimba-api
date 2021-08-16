96b547ee3786a0354d80fd2788d5818dc50f2b8c
kaguna <karyorkir@gmail.com>
2021-08-16 21:46:10 +0300

    refactor league list

--- app/serializers/league_serializer.rb
+++ app/serializers/league_serializer.rb
@@ -1,6 +1,10 @@
 class LeagueSerializer < ActiveModel::Serializer
   attributes :id, :title, :official, :current_season
 
+  def title
+    object.title.capitalize
+  end
+  
   def current_season
     object.seasons.current.first
   end

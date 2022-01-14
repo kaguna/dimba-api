ac3c5c14b0cab9263d59745078a80dc4dd8c44b4
kaguna <karyorkir@gmail.com>
2022-01-14 16:10:14 +0300

    add league seasons winner in archive summary

--- app/serializers/season_serializer.rb
+++ app/serializers/season_serializer.rb
@@ -1,6 +1,15 @@
 class SeasonSerializer < ActiveModel::Serializer
   attributes :id, :name, :current
+  attribute :season_winner, if: :not_current
 
   belongs_to :league
   has_many :fixtures
+
+  def not_current
+    !object.current
+  end
+
+  def season_winner
+    JSON.parse(AllResult.standings(object.league_id, object.id))['standing'][0]
+  end
 end

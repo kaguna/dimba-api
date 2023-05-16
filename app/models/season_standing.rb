9e47fceb40d164421756ff40480b02d69d5a3ca9
kaguna <karyorkir@gmail.com>
2023-05-16 12:38:42 +0300

    fix styling issues

--- app/models/season_standing.rb
+++ app/models/season_standing.rb
@@ -4,24 +4,29 @@ class SeasonStanding < ApplicationRecord
   has_one :season
 
   def self.archived_season(season_id:, tab:)
-    tab ==='table' ? archived_standing(season_id: season_id) : archived_scorers(season_id: season_id)
+    if tab === 'table'
+      archived_standing(season_id: season_id)
+    else
+      archived_scorers(season_id: season_id)
+    end
   end
+  class << self
+    private
 
-  private
+    def archived_standing(season_id:)
+      season = get_season(season_id: season_id)
+      season_standing = season.present? ? season.standing : { teams: 0, standing: [] }.to_json
+      JSON.parse(season_standing)
+    end
 
-  def self.archived_standing(season_id:)
-    season = self.get_season(season_id: season_id)
-    season_standing = season.present? ? season.standing : { teams: 0, standing: [] }.to_json
-    JSON.parse(season_standing)
-  end
+    def archived_scorers(season_id:)
+      season = get_season(season_id: season_id)
+      season_scorers = season.present? && !season.scorers.nil? ? season.scorers : { scorers: [] }.to_json
+      JSON.parse(season_scorers)
+    end
 
-  def self.archived_scorers(season_id:)
-    season = self.get_season(season_id: season_id)
-    season_scorers = season.present? && !season.scorers.nil? ? season.scorers : { scorers: [] }.to_json
-    JSON.parse(season_scorers)
-  end
-
-  def self.get_season(season_id:)
-    find_by(season_id: season_id)
+    def get_season(season_id:)
+      find_by(season_id: season_id)
+    end
   end
 end

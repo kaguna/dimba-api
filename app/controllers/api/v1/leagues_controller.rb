0ff326f7c131514c9c0171efe40ddc98023e0d75
kaguna <karyorkir@gmail.com>
2023-08-21 14:53:38 +0300

    order favourited leagues first(mobile first)

--- app/controllers/api/v1/leagues_controller.rb
+++ app/controllers/api/v1/leagues_controller.rb
@@ -1,30 +1,33 @@
+# frozen_string_literal: true
+
 module Api
-  module V1   
+  module V1
     class LeaguesController < ApplicationController
       before_action :authenticate_current_user!, except: %i[index show]
       before_action :set_league, only: %i[show update destroy]
       after_action :verify_authorized, except: %i[index show]
 
       def index
-        leagues = League.active
+        active_leagues = League.active
+        leagues = current_user.present? && current_user.email_confirmed ? League.ordered_by_favourites(current_user: current_user) : active_leagues
         all_leagues = leagues.all_paginated_leagues(per_page: params[:per_page].to_i, page: params[:page].to_i)
         if leagues.empty?
           render json: { "error": 'No leagues!' },
-                status: :not_found
+                 status: :not_found
 
         else
           render json: {
-                 leagues: ActiveModelSerializers::SerializableResource.new(all_leagues, 
-                          each_serializer: LeagueSerializer,
-                          scope: { current_user: current_user, show: "all" }
-                        ),
-                 total: leagues.count
-                }, status: :ok
+            leagues: ActiveModelSerializers::SerializableResource.new(all_leagues,
+                                                                      each_serializer: LeagueSerializer,
+                                                                      scope: { current_user: current_user,
+                                                                               show: 'all' }),
+            total: active_leagues.count
+          }, status: :ok
         end
       end
 
       def show
-        render json: @league, scope: { current_user: current_user, show: "details" }
+        render json: @league, scope: { current_user: current_user, show: 'details' }
       end
 
       def create
